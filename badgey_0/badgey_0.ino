7033d77b6ad900c1161b17055a79b2b8ae1c0205
Kerry Scharfglass <iborgel@gmail.com>
2014-09-24 00:17:31 -0700

    drawing characters ontop of eachother works fine

--- badgey_0/badgey_0.ino
+++ badgey_0/badgey_0.ino
@@ -1,15 +1,82 @@
-// Simple LED blink
+
+/*************************************************** 
+  This is a library for our I2C LED Backpacks
+
+  Designed specifically to work with the Adafruit LED Matrix backpacks 
+  ----> http://www.adafruit.com/products/872
+  ----> http://www.adafruit.com/products/871
+  ----> http://www.adafruit.com/products/870
+
+  These displays use I2C to communicate, 2 pins are required to 
+  interface. There are multiple selectable I2C addresses. For backpacks
+  with 2 Address Select pins: 0x70, 0x71, 0x72 or 0x73. For backpacks
+  with 3 Address Select pins: 0x70 thru 0x77
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include <Adafruit_LEDBackpack.h>
+#include <Adafruit_GFX.h>
+
+Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 
 const int led = LED_BUILTIN;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("8x8 LED Matrix Test");
+  
+  matrix.begin(0x70);  // pass in the address
+  matrix.clear();
+  
+  //onboard LED
   pinMode(led, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(1000);
-}
+int state = 0;
 
+void loop() {
+  matrix.setTextSize(1);
+  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextColor(LED_ON);
+  
+    matrix.clear();
+    matrix.setCursor(-1,1);
+    matrix.print(":");
+    matrix.setCursor(3,1);
+    matrix.print(")");
+    matrix.writeDisplay();
+
+
+    delay(1000);
+    
+    state = (state == LOW) ? HIGH : LOW;
+    digitalWrite(led, state);
+  
+  /*
+  for (int8_t x=0; x>=-36; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("Hello World");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  
+  
+  matrix.setRotation(3);
+  for (int8_t x=7; x>=-36; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("World");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  matrix.setRotation(0);
+  */
+}
